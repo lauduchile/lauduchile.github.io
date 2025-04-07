45c59eeac16a337dea9437c871f4d94e800c4ead
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-04-07 00:21:51 -0400

    agregar template de 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Lutheria PostDigital",
+	url: "https://example.com/",
+	language: "es",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Lutheria PostDigital",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
